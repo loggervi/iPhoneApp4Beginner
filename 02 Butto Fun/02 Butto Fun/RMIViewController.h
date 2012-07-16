ea88a8907e7fdad51d4be5db9b8ad2fd648c3dfe
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-16 17:01:14 +0200

    Initial project import

---
+++ 02 Butto Fun/02 Butto Fun/RMIViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMIViewController.h
+//  02 Butto Fun
+//
+//  Created by Reto Misteli on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMIViewController : UIViewController
+
+@end
