9b13b4c5b231136956a057b5c8cd30a6a71dd5c2
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-16 16:17:28 +0200

    Initial project commit

---
+++ HelloWorld/HelloWorld/RMIAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RMIAppDelegate.h
+//  HelloWorld
+//
+//  Created by Reto Misteli on 16.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RMIViewController;
+
+@interface RMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RMIViewController *viewController;
+
+@end
