c8d03b8a7a3727b900195a18f752955ef3d7707a
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-17 13:34:50 +0200

    Initial project commit

---
+++ 05 Autosize/05 Autosize/RMIAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RMIAppDelegate.h
+//  05 Autosize
+//
+//  Created by Reto Misteli on 17.07.12.
+//  Copyright (c) 2012 rmisteli.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RMIViewController;
+
+@interface RMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RMIViewController *viewController;
+
+@end
---
+++ 05 Autosize/05 Autosize/RMIViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMIViewController.h
+//  05 Autosize
+//
+//  Created by Reto Misteli on 17.07.12.
+//  Copyright (c) 2012 rmisteli.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMIViewController : UIViewController
+
+@end
