36b31228f5a013b89a4bc23362f24779d048f345
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-17 13:04:08 +0200

    Added button

--- 04 Controller Fun/04 Controller Fun/RMIViewController.h
+++ 04 Controller Fun/04 Controller Fun/RMIViewController.h
@@ -8,14 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RMIViewController : UIViewController
+@interface RMIViewController : UIViewController <UIActionSheetDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
 
 - (IBAction)textFieldDoneEditing:(id)sender;
 - (IBAction)backgroundTap:(id)sender;
 - (IBAction)sliderChanged:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+- (IBAction)toggleControls:(id)sender;
+- (IBAction)duSomethingButtonPressed:(id)sender;
 
 @end
--- 04 Controller Fun/04 Controller Fun/RMIViewController.m
+++ 04 Controller Fun/04 Controller Fun/RMIViewController.m
@@ -12,6 +12,9 @@
 @synthesize nameField;
 @synthesize numberField;
 @synthesize sliderLabel;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize doSomethingButton;
 
 - (void)didReceiveMemoryWarning
 {
@@ -25,6 +28,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage *buttonImageNormal = [UIImage imageNamed:@"whiteButton.png"];
+    UIImage *stretchableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImageNormal forState:UIControlStateNormal];
+    
+    UIImage *buttonImagePressed = [UIImage imageNamed:@"blueButton.png"];
+    UIImage *stretchableButtonImagePressed = [buttonImagePressed stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [doSomethingButton setBackgroundImage:stretchableButtonImagePressed forState:UIControlStateHighlighted];
 }
 
 - (void)viewDidUnload
@@ -32,6 +42,9 @@
     [self setNameField:nil];
     [self setNumberField:nil];
     [self setSliderLabel:nil];
+    [self setLeftSwitch:nil];
+    [self setRightSwitch:nil];
+    [self setDoSomethingButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -76,6 +89,46 @@
     UISlider *slider = (UISlider *)sender;
     int progressAsInt = (int)roundf(slider.value);
     sliderLabel.text = [NSString stringWithFormat:@"%d", progressAsInt];
+}
+
+- (IBAction)switchChanged:(id)sender {
+    UISwitch *whichSwitch = (UISwitch *)sender;
+    BOOL setting = whichSwitch.isOn;
+    [leftSwitch setOn:setting animated:YES];
+    [rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)toggleControls:(id)sender {
+    // 0 == index for switches
+    if([sender selectedSegmentIndex] == 0) {
+        leftSwitch.hidden = NO;
+        rightSwitch.hidden = NO;
+        doSomethingButton.hidden = YES;
+    } else {
+        leftSwitch.hidden = YES;
+        rightSwitch.hidden = YES;
+        doSomethingButton.hidden = NO;
+    }
+}
+
+- (IBAction)duSomethingButtonPressed:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:@"No Way!" delegate:self cancelButtonTitle:@"No Way!" destructiveButtonTitle:@"Yes, I'm sure!" otherButtonTitles:nil, nil];
+    [actionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if(buttonIndex != [actionSheet cancelButtonIndex]) {
+        NSString *msg = nil;
+        if (nameField.text.length > 0) {
+            msg = [[NSString alloc] initWithFormat: @"You can breathe easy, %@, everything went OK.", nameField.text];
+        }
+        else {
+            msg = @"You can breathe easy, everything went OK.";
+        }
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something was done" message:msg delegate:self cancelButtonTitle:@"Phew!" otherButtonTitles:nil, nil];
+        [alert show];
+    }
 }
 
 @end
