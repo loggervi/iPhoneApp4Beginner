86bdbf4940e7872bcdd197ae1481ecdd7b6b80ed
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-17 09:45:04 +0200

    Initial project import

---
+++ 04 Controller Fun/04 Controller Fun/RMIAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  RMIAppDelegate.h
+//  04 Controller Fun
+//
+//  Created by Reto Misteli on 17.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RMIViewController;
+
+@interface RMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RMIViewController *viewController;
+
+@end
