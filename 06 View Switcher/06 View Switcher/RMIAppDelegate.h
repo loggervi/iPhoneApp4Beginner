3593d359db767aad57dfd3266e7613766c4ffe2f
rmisteli <reto.misteli@students.fhnw.ch>
2012-07-18 07:51:13 +0200

    Initial project commit

---
+++ 06 View Switcher/06 View Switcher/RMIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMIAppDelegate.h
+//  06 View Switcher
+//
+//  Created by Reto Misteli on 18.07.12.
+//  Copyright (c) 2012 rmisteli.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
